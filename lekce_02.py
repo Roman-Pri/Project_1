1a372dbc75e37adf73950f24c89e240b68c43d59
Roman-Pri <roman.prihonsky83@gmail.com>
2024-11-28 20:27:59 +0100

    new

--- lekce_02.py
+++ lekce_02.py
@@ -1,17 +1,9 @@
-zadani = input ("Zadej jmeno:")
-jmeno = ["Marek", "Petr", "Slon"]
-
-if jmeno == ["Marek", "Petr", "Slon"]:
- print("Ahoj, Marku!")
+cislo= int (input ("zadej cislo: "))
+if cislo % 3 == 0 and cislo % 5 == 0:
+    print ("fizzBuzz")
+elif cislo % 3 == 0:
+    print ("fizz")
+elif cislo % 5 == 0:
+    print ("Buzz")
 else:
- print("Ahoj, vsem ostatnim!")
-
- 
-muj_list = [1, 3, 4, 5]
-muj_list.insert(2, 2)
-print(muj_list)
-
-
-print ([2,3,4,5] [3])
-seznam_05= list []
-
+    print ("spatne",cislo)
