1bf08847b4b59292c317c97f24e032db9a0765ca
Roman-Pri <roman.prihonsky83@gmail.com>
2025-01-16 21:59:50 +0100

    16.1.2025 GUI

--- Projekty_engeto/projekt_2.py
+++ Projekty_engeto/projekt_2.py
@@ -4,13 +4,18 @@ projekt_2.py: druhý projekt do Engeto Online Python Akademie
 Ing. Roman Příhonský, MMS, MSS
 email:  roman.prihonsky83@gmail.com
 """
-# Intial data
+# Imported library
 import random           # podpora pro tvorbu nahodneho cisla
 import datetime         # podpora pro mereni delky hry
 import tkinter          # GUI
-import pandas as pd     # uchovani statistik
+import pandas           # uchovani statistik
 import matplotlib       # zobrazeni statistik
 
+# Functions definition
+def run_new_game():
+    for widget in window.winfo_children():
+        widget.destroy()
+        
 # 1.GUI
 # 1.1 Window
 window = tkinter.Tk()
@@ -25,7 +30,7 @@ label = tkinter.Label(window, text = "Welcome to BULLs & COWs game", font = ("Ar
 label.pack(pady = 10)
 
 # 1.3 button "NG"
-button = tkinter.Button(window, text = "NEW GAME", font = ("Arial", 20), width = 15, height= 2) #chybi definovat command (command = button_click)
+button = tkinter.Button(window, text = "NEW GAME", font = ("Arial", 20), width = 15, height = 2, command = run_new_game) 
 button.place(x = 525, y = 200)
 
 # 1.4 button "GS"
@@ -35,5 +40,6 @@ button.place(x = 525, y = 350)
 # 1.5 button "Opt"
 button = tkinter.Button(window, text = "OPTIONS", font = ("Arial", 20), width = 15, height= 2) #chybi definovat command (command = button_click)
 button.place(x = 525, y = 500)
+
 
 window.mainloop()
